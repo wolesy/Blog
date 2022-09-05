4627b4919c23948763767d62c75776de19285596
Cameron Scholes <cameron.scholes@hotmail.com>
2022-09-05 20:13:58 +0100

    Creation

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html lang="en">
+
+<html>
+  <head>
+  <title>Blog Example | Cameron Scholes</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, inital-scale=1" />
+  <meta name="viewport" content="height=device-height, inital-scale=1" />
+  <meta name="author" content="Cameron Scholes" />
+  <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <header>
+      <h1 class="header-text">Joe's Blog</h1>
+    </header>
+    <main>
+      <div class="blog-post">
+        <h2 class="post-header">This is a post</h2>
+        <h3 class="post-subheader">Posted by <span class="post-poster">Cameron Scholes</span> on <span class="post-date">05/09/2022</span></h3>
+      </div>
+    </main>
+  </body>
+</html>
